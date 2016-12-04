d3bde000f69bed8f7f1dda20954986f05decafff
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2016-12-03 19:43:06 -0800

    Create Avatar component consisting of 3 child components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+  name: 'Ruxandra F.',
+  username: 'ruxandrafed',
+  image: 'https://avatars1.githubusercontent.com/u/11021586?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+var ProfilePic = React.createClass({
   render: function () {
     return (
-      <div>Hello ReactJS Program!</div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    )
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <Hello />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
