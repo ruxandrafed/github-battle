69cfb7b3b5e3f054af611f3605c1531cf5687de1
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-10 19:25:02 -0800

    Create PromptContainer and update routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,7 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
+var PromptContainer = require('../containers/PromptContainer');
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
@@ -12,6 +13,8 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
     </Route>
   </Router>
 );
