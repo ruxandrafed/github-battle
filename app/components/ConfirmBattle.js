c9e9abf959f8bdce9564b5ccb0a3f87a59098973
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-10 23:30:43 -0800

    Dump props on battle page

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,14 @@
 var React = require('react');
 
+function puke(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 var ConfirmBattle = React.createClass({
   render: function() {
     return (this.props.isLoading === true)
       ? (<p> LOADING! </p>)
-      : (<p> Confirm Battle </p>)
+      : (<p> Confirm Battle {puke(this.props)} </p>)
   }
 });
 
