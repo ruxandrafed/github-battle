802359e4bd61639c1f527631c9f23f2f9d1d74ab
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-15 21:43:06 -0800

    Complete results page

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,46 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var Link = require('react-router').Link;
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
 }
 
 function Results(props) {
+  if (props.scores[0] === props.scores[1]) {
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>It's a tie!</h1>
+        <div className="col-sm-12" style={styles.space}>
+          <Link to="/playerOne">
+            <button type="button" className="btn btn-lg btn-success">Start over</button>
+          </Link>
+        </div>
+      </div>
+      )
+  }
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div>Results: {puke(props)}</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}/>
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}/>
+        </UserDetailsWrapper>
+      </div>
+      <div className="col-sm-12" style={styles.space}>
+        <Link to="/playerOne">
+          <button type="button" className="btn btn-lg btn-success">Start over</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
