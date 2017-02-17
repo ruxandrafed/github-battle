16d23e55e25d2f1c2bf624fbfa5138af9d02cd2d
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-17 10:08:01 -0800

    Refactor ResultsContainer componentDidMount to use async/await

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -10,17 +10,28 @@ const ResultsContainer = React.createClass({
       scores: []
     }
   },
-  componentDidMount () {
-//    we pass state in handleInitiateBattle
-//    console.log(this.props.location.state.playersInfo);
-    battle(this.props.location.state.playersInfo)
-      .then((scores) => {
-        this.setState({
-          scores: scores,
-          isLoading: false
-        })
+  async componentDidMount() {
+    try {
+      const scores = await battle(this.props.location.state.playersInfo);
+      this.setState({
+        scores: scores,
+        isLoading: false
       });
+    } catch (error) {
+      console.warn('Error in ResultsContainer: ', error);
+    }
   },
+//  componentDidMount () {
+////    we pass state in handleInitiateBattle
+////    console.log(this.props.location.state.playersInfo);
+//    battle(this.props.location.state.playersInfo)
+//      .then((scores) => {
+//        this.setState({
+//          scores: scores,
+//          isLoading: false
+//        })
+//      });
+//  },
   render () {
     return (
       <Results
